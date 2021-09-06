2976bd8db4c58308c0f8422244b8b43736d6d9e4
Tanner <tannercdolby@gmail.com>
2021-09-05 23:14:12 -0700

    update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 const metagen = require("eleventy-plugin-metagen");
 const CleanCSS = require("clean-css");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const { minify } = require("terser");
 
 module.exports = (eleventyConfig) => {
 
-    eleventyConfig.addPassthroughCopy("./src/_includes/css")
+    eleventyConfig.addPassthroughCopy("./src/_includes/css");
+    eleventyConfig.addPassthroughCopy("./src/_includes/js");
 
     eleventyConfig.addPlugin(metagen);
     eleventyConfig.addPlugin(CleanCSS);
@@ -14,8 +16,26 @@ module.exports = (eleventyConfig) => {
         templateFormats: ["njk", "md"],
     });
 
+    eleventyConfig.addNunjucksAsyncFilter("jsmin", async function (
+        code,
+        callback
+      ) {
+        try {
+          const minified = await minify(code);
+          callback(null, minified.code);
+        } catch (err) {
+          console.error("Terser error: ", err);
+          // Fail gracefully.
+          callback(null, code);
+        }
+      });
+
     eleventyConfig.addFilter("cssmin", function(code) {
         return new CleanCSS({}).minify(code).styles;
+    });
+
+    eleventyConfig.addFilter("date", function(date) {
+        return date.toDateString();
     });
 
     eleventyConfig.addShortcode('getYear', function() {
